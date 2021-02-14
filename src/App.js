3e8b8f3359778101ae5732625e79b9ab89c2651f
Rocio del Pilar Esteban <hr.chio@gmail.com>
2021-02-14 11:19:42 -0300

    se agrega el componente con la lista de imagenes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar";
 import Carousel from "./components/Carousel";
+import ListContainer from "./container/ListContainerCard";
 function App() {
   return (
     <div className='App'>
       <Navbar />
       <Carousel />
+      <ListContainer />
     </div>
   );
 }
