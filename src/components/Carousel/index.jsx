7a5a89550819c94a22fdf543a7d44bf83d6a6673
Rocio del Pilar Esteban <hr.chio@gmail.com>
2021-02-14 11:21:39 -0300

    se da estilo para que carousel tome un tamaño específico de las imagenes

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -15,21 +15,36 @@ function ControlledCarousel() {
   return (
     <Carousel activeIndex={index} onSelect={handleSelect}>
       <Carousel.Item>
-        <img className='d-block w-100' src={img2} alt='First slide' />
+        <img
+          className='d-block w-100'
+          src={img2}
+          alt='First slide'
+          height='450px'
+        />
         <Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img className='d-block w-100' src={img1} alt='Second slide' />
+        <img
+          className='d-block w-100'
+          src={img1}
+          alt='Second slide'
+          height='450px'
+        />
         <Carousel.Caption>
           <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img className='d-block w-100' src={img3} alt='Third slide' />
+        <img
+          className='d-block w-100'
+          src={img3}
+          alt='Third slide'
+          height='450px'
+        />
 
         <Carousel.Caption>
           <h3>Third slide label</h3>
