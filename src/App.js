6d23641ee95edd87be561a74a1f77902d01a0964
Rocio del Pilar Esteban <hr.chio@gmail.com>
2021-02-15 10:38:03 -0300

    se agrega browserRouter, switch y route  en app en los container correspondiente

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import Navbar from "./components/Navbar";
 import Carousel from "./components/Carousel";
-import ListContainer from "./container/ListContainerCard";
+import ListContainer from "./container/ListContainerCard/ListContainer";
+import SearchListContainer from "./container/ListContainerCard/SearchListContainer";
+import CategoryListContainer from "./container/ListContainerCard/CategoryListContainer";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
 function App() {
   return (
-    <div className='App'>
+    <BrowserRouter>
       <Navbar />
       <Carousel />
-      <ListContainer />
-    </div>
+      <Switch>
+        <Route exact path='/'>
+          <ListContainer />
+        </Route>
+        <Route exact path='/categories/:keyword'>
+          <CategoryListContainer />
+        </Route>
+        <Route exact path='/search/:searchWord'>
+          <SearchListContainer />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
