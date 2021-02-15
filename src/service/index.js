5e8ad3c92385a9dc5fbaa62d7d062b4afcf2b436
Rocio del Pilar Esteban <hr.chio@gmail.com>
2021-02-15 10:36:33 -0300

    se agrega el enpoint de search/category

--- src/service/index.js
+++ src/service/index.js
@@ -1,4 +1,7 @@
 const api_key = process.env.REACT_APP_API_KEY_UNPLASH;
-console.log("api", api_key);
-console.log("pross", process.env);
+
+//Enpoint para que nos devuelva todos las fotos:
 export const api_photos = `https://api.unsplash.com/photos/?page=1&client_id=${api_key}`;
+
+//Enpoint para que nos filtre por categoría:
+export const api_search = `https://api.unsplash.com/search/photos?query=[search]&client_id=${api_key}`;
